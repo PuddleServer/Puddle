582097ae02c73bb30f171a9b6f6d75189361ae6b
KINGVOXY <daruo0830@gmail.com>
2021-08-31 23:50:28 +0900

    Systemクラステストの生成まで

--- test/System_Test.ts
+++ test/System_Test.ts
@@ -1,21 +1,43 @@
 /**
- * System.tsのテストファイル
+ * System.tsのSystemクラステストファイル
  * @author Daruo(KINGVOXY)
  * @author AO2324(AO2324-00)
- * @Date   2021-08-28
+ * @Date   2021-08-31
  */
 
 import { assertEquals }     from "https://deno.land/std@0.88.0/testing/asserts.ts";
 import { System }           from "../System.ts";
 
+class Apple {
+
+    #variety: string;
+    #prise: number;
+
+    constructor(variety: string, prise: number) {
+        this.#variety = variety;
+        this.#prise = prise;
+    }
+
+    getVariety(): string {
+        return this.#variety;
+    }
+
+    getPrise(): number {
+        return this.#prise;
+    }
+}
+
 /**
  * テスト
  */
- Deno.test({
-    name: "テスト",
+
+Deno.test({
+    name: "生成テスト",
     fn(): void {
+        const apple = new Apple("Fuji", 100);
         
-        // write here.
+        const system1 = new System();
+        const system2 = new System(apple);
 
     },
 });
