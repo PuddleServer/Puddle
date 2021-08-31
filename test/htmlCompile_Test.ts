9c3ba7dcc3149693aa3695e1323627876a225f6a
KINGVOXY <daruo0830@gmail.com>
2021-08-31 21:30:25 +0900

    ファイル名変更・テストの記述

---
+++ test/htmlCompile_Test.ts
@@ -0,0 +1,47 @@
+/**
+ * HtmlCompiler.tsのhtmlCompile関数テストファイル
+ * @author Daruo(KINGVOXY)
+ * @author AO2324(AO2324-00)
+ * @Date   2021-08-31
+ */
+
+ import { assertEquals }     from "https://deno.land/std@0.88.0/testing/asserts.ts";
+ import { htmlCompile }            from "../HtmlCompiler.ts";
+ 
+ const html_before: String = `
+ <html>{{ header }}</html>
+ <html>{{ contents }}</html>
+ <html>{{header}}</html>
+ <html>{{contents}}</html>
+ {{{header}}}
+ {contents}
+ \${header}
+ {{contents}abc}
+ (header)
+ <contents>
+ `;
+ 
+ const answer: String = `
+ <html>タイトル</html>
+ <html>本文</html>
+ <html>タイトル</html>
+ <html>本文</html>
+ {タイトル}
+ {contents}
+ \${header}
+ {{contents}abc}
+ (header)
+ <contents>
+ `;
+ 
+ /**
+  * htmlCompile関数テスト
+  */
+ Deno.test({
+     name: "htmlCompile関数テスト",
+     fn(): void {
+         const html_after = htmlCompile(html_before, {header: "タイトル",contents: "本文"});
+         assertEquals(true, html_after == answer)
+ 
+     },
+ });
