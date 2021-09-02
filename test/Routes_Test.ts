61ed35cbd1382a97514fd8e008ca4c90db2005e2
KINGVOXY <daruo0830@gmail.com>
2021-09-02 20:56:36 +0900

    生成テストまで

--- test/Routes_Test.ts
+++ test/Routes_Test.ts
@@ -6,16 +6,19 @@
  */
 
 import { assertEquals } from "https://deno.land/std@0.88.0/testing/asserts.ts";
-import { Routes } from "../Router.ts";
+import { Route, Routes } from "../Router.ts";
 
 /**
- * テスト
+ * 生成テスト
  */
 Deno.test({
-    name: "テスト",
+    name: "生成テスト",
     fn(): void {
-        
-        // write here.
+        const route: Route = new Route("/index.html");
+
+        const routes1: Routes = new Routes();
+        const routes2: Routes = new Routes(route);
+        const routes3: Routes = new Routes(route, route);
 
     },
 });
