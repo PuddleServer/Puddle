0d0ceb14e5dead02bdf0768dcb537ac0cf09e9c8
KINGVOXY <daruo0830@gmail.com>
2021-09-07 23:09:32 +0900

    [control_Test] Controller.tsの関数テストファイルを作成

---
+++ test/control_Test.ts
@@ -0,0 +1,21 @@
+/**
+ * Controller.ts control関数のテストファイル
+ * @author Daruo(KINGVOXY)
+ * @author AO2324(AO2324-00)
+ * @Date   2021-09-07
+ */
+
+import { assertEquals }     from "https://deno.land/std@0.88.0/testing/asserts.ts";
+import { control }          from "../Controller.ts";
+
+/**
+ * controlテスト
+ */
+ Deno.test({
+    name: "controlテスト",
+    fn(): void {
+
+        // write here
+
+    },
+});
