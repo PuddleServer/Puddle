ac9226bd78be9bbffc67d3e2bd8c32300467d357
KINGVOXY <daruo0830@gmail.com>
2021-09-09 23:32:47 +0900

    基本的なテストの実装

--- test/System_Test.ts
+++ test/System_Test.ts
@@ -6,9 +6,9 @@
  */
 
 import { assertEquals }     from "https://deno.land/std@0.88.0/testing/asserts.ts";
-import { System }           from "../System.ts";
+import { System, Route }           from "../mod.ts";
 
-class Apple {
+class Fruit {
 
     #variety: string;
     #prise: number;
@@ -28,16 +28,122 @@ class Apple {
 }
 
 /**
- * テスト
+ * setModuleテスト
  */
-
 Deno.test({
-    name: "生成テスト",
+    name: "setModuleテスト",
     fn(): void {
-        const apple = new Apple("Fuji", 100);
-        
-        const system1 = new System();
-        const system2 = new System(apple);
+        const apple: Fruit = new Fruit("apple", 200);
+        System.setModule("リンゴ", apple);
+    },
+});
 
+/**
+ * setModulesテスト
+ */
+Deno.test({
+    name: "setModulesテスト",
+    fn(): void {
+        const orange: Fruit = new Fruit("orange", 20);
+        const grape : Fruit = new Fruit("grape", 3000);
+        System.setModules({"オレンジ": orange, "ブドウ": grape});
+    },
+});
+
+/**
+ * setModuleテスト
+ */
+Deno.test({
+    name: "getModuleテスト",
+    fn(): void {
+        assertEquals(200, System.getModule("リンゴ").getPrise());
+        assertEquals(3000, System.getModule("ブドウ").getPrise());
+    },
+});
+
+/**
+ * deleteModuleテスト
+ */
+Deno.test({
+    name: "deleteModuleテスト",
+    fn(): void {
+        System.deleteModule("林檎");
+        assertEquals(undefined, System.getModule("林檎"));
+    },
+});
+
+/**
+ * createRouteテスト
+ */
+Deno.test({
+    name: "createRouteテスト",
+    fn(): void {
+        // 引数がstringの場合
+        const route1: Route = System.createRoute("/index.html");
+        assertEquals("/index.html", route1.PATH());
+
+        // 引数がRouteの場合
+        const r: Route = new Route("/about.html");
+        const route2: Route = System.createRoute(r);
+        assertEquals("/about.html", route2.PATH())
+        
+    },
+});
+
+/**
+ * createRoutesテスト
+ */
+Deno.test({
+    name: "createRoutesテスト",
+    async fn(): Promise<void> {
+        const r1: Route = new Route("/crs1.html")
+        const r2: Route = new Route("/crs2.html")
+        
+        const routes: { [key: string]: Route; } = await System.createRoutes(r1, r2, "/crs3.html");
+        const routes_none: { [key: string]: Route; } = await System.createRoutes();
+
+        assertEquals("/crs1.html", routes["/crs1.html"].PATH());
+        assertEquals("/crs2.html", routes["/crs2.html"].PATH());
+        assertEquals("/crs3.html", routes["/crs3.html"].PATH());
+        assertEquals({}, routes_none);
+    },
+});
+
+/**
+ * Routeテスト
+ */
+Deno.test({
+    name: "Routeテスト",
+    fn(): void {
+        const get: Function = () => {};
+        System.Route("/about.html").GET(get, true);
+        System.Route("/crs1.html").GET(get, true);
+        System.Route("/crs2.html").GET(get, true);
+        assertEquals(true, System.Route("/about.html").isWebSocket);
+        assertEquals(true, System.Route("/crs1.html").isWebSocket);
+        assertEquals(true, System.Route("/crs2.html").isWebSocket);
+    },
+});
+
+/**
+ * deleteRouteテスト
+ */
+Deno.test({
+    name: "deleteRouteテスト",
+    fn(): void {
+        System.deleteRoute("/about.html");
+        assertEquals(false, System.Route("/about.html").isWebSocket);
+    },
+});
+
+/**
+ * deleteRoutesテスト
+ */
+Deno.test({
+    name: "deleteRoutesテスト",
+    fn(): void {
+        System.deleteRoutes(["/crs1.html", "/crs2.html"]);
+        assertEquals(false, System.Route("/crs1.html").isWebSocket);
+        assertEquals(false, System.Route("/crs2.html").isWebSocket);
     },
 });
