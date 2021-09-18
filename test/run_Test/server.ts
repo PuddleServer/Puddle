ac75c4702f9bf84f31d74f081a5d57e689775837
KINGVOXY <daruo0830@gmail.com>
2021-09-18 22:43:04 +0900

    🐈

--- test/run_Test/server.ts
+++ test/run_Test/server.ts
@@ -1,7 +1,8 @@
 import { System, Config, default_get, default_error, redirect } from "../../mod.ts"
 
-System.createRoute("./assets/index.html").URL("/", "/get").GET(default_get);
+System.createRoute("./assets/index.html").URL("/", "/get").GET(redirect("http://localhost:8080/post"));
+System.createRoute("./assets/post.html").URL("/post")
 
-System.listen((conf: Config)=>{
+System.listen(8080, (conf: Config)=>{
     console.log(`The server running on http://${conf.hostname}:${conf.port}`);
 });
