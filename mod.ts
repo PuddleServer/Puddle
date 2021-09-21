fec41583d9b88bd88387b1df451964f71fdbe6d9
BuildTools <unconfigured@null.spigotmc.org>
2021-09-21 22:40:49 +0900

    Logger.ts を実装

--- mod.ts
+++ mod.ts
@@ -15,7 +15,11 @@ export {
 } from "https://deno.land/std@0.106.0/ws/mod.ts"
 export type { WebSocket } from "https://deno.land/std@0.106.0/ws/mod.ts"
 export { lookup } from "https://deno.land/x/mime_types@1.0.0/mod.ts"
-
+export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
+/*export {
+    ensureFile
+} from "https://deno.land/std@0.106.0/fs/mod.ts";
+*/
 export * from "./System.ts"
 export * from "./SystemResponse.ts"
 export * from "./Router.ts"
