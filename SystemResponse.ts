ca4dd6a7698fdadeefb1f620f8f9d166ba0683ad
BuildTools <unconfigured@null.spigotmc.org>
2021-10-04 22:59:22 +0900

    [ SystemResponse.ts ] コメントの修正。

--- SystemResponse.ts
+++ SystemResponse.ts
@@ -1,9 +1,3 @@
-/**
- * Serverのresponse関連の機能をまとめたクラス。
- * @author Daruo(KINGVOXY)
- * @author AO2324(AO2324-00)
- * @Date   2021-09-17
- */
 import {
     ServerRequest, Response,
     Cookie, setCookie,
@@ -11,22 +5,42 @@ import {
     htmlCompile
 } from "./mod.ts"
 
+/**
+ * クライアントへのレスポンス機能を拡張するクラス。
+ * Class that extends the response functionality to the client.
+ */
 export class SystemResponse {
 
-    /** requestを格納する変数 */
+    /**
+     * リクエストオブジェクトを格納する変数。
+     * Variable that stores the Request object.
+     */
     #request: ServerRequest;
 
-    /** presetを格納する変数 */
+    /**
+     * HTML内の変数に挿入するテキストを格納する連想配列。
+     * An associative array that stores text to be inserted into variables in HTML.
+     */
     #preset: { [key: string]: any; };
 
-    /** レスポンスオブジェクト */
+    /**
+     * レスポンスオブジェクトを格納する変数。
+     * Variable that stores the Response object.
+     */
     response: Response;
 
+    /**
+     * ヘッダーを格納する変数。
+     * Variable that stores the Headers object.
+     */
     headers: Headers;
     status: number;
     body: string;
 
-    /** 強制ダウンロードかどうか（初期値はfalse） */
+    /**
+     * 強制ダウンロードかどうか（初期値はfalse）。
+     * Whether to force download or not (default value is false).
+     */
     isForceDownload: boolean;
 
     constructor(request: ServerRequest) {
@@ -47,10 +61,11 @@ export class SystemResponse {
     }
 
     /**
-     * Responseオブジェクトにテキストを設定する。
-     * @param text クライアントに返す文字列。
-     * @param status ステータスコード（デフォルトは200）。
-     * @param statusText ステータステキスト。
+     * レスポンスオブジェクトにテキストを設定する。
+     * Set the response object to text.
+     * @param text A string to return to the client.
+     * @param status Status code (default is 200).
+     * @param statusText Status text.
      */
     setText(text: string, status: number = 200, statusText: string | null = null): void {
         this.body = htmlCompile(text, this.#preset);
@@ -61,10 +76,11 @@ export class SystemResponse {
     }
 
     /**
-     * Responseオブジェクトにファイルを設定する。
-     * @param filePath クライアントに返すファイルのパス。
-     * @param status ステータスコード（デフォルトは200）。
-     * @param statusText ステータステキスト。
+     * レスポンスオブジェクトにファイルを設定する。
+     * Set the file to the Response object.
+     * @param filePath The path of the file to return to the client.
+     * @param status Status code (default is 200).
+     * @param statusText Status text.
      */
     async setFile(filePath: string, status: number = 200, statusText: string | null = null): Promise<void> {
         const file = await Deno.open(filePath);
@@ -85,7 +101,8 @@ export class SystemResponse {
 
     /**
      * セットしたファイルや文字列に変数が埋め込まれていた場合に、参照されるオブジェクトを定義する。
-     * @param object 参照される変数を格納したオブジェクト。
+     * Defines an object to be referenced when a variable is embedded in a set file or string.
+     * @param object An object that contains a variable to be referenced.
      */
     preset(object: { [key: string]: any; }): void {
         this.#preset = object;
@@ -93,7 +110,14 @@ export class SystemResponse {
 
     /**
      * Cookieのセットを行う。
-     * @param cookie 
+     * Set cookies.
+     * @param cookie Cookie object.
+     * ```ts
+     * response.setCookie({
+     *      name: 'deno', value: 'runtime',
+     *      httpOnly: true, secure: true, maxAge: 2, domain: "deno.land"
+     * });
+     * ```
      */
     setCookie(cookie: Cookie): void {
         setCookie(this.response, cookie);
@@ -101,7 +125,8 @@ export class SystemResponse {
 
     /**
      * Cookieの削除を行う。
-     * @param name 削除するCookie名。
+     * Delete cookie.
+     * @param name Cookie name.
      */
     deleteCookie(
         name: string,
@@ -116,7 +141,8 @@ export class SystemResponse {
     }
 
     /**
-     * ServerRequestのrespondを実行する。
+     * クライアントにレスポンスを返して処理を終了する。
+     * Return the response to the client and finish the process.
      */
     send(response?: string | Response): void {
         if(this.isForceDownload) {
@@ -130,8 +156,9 @@ export class SystemResponse {
     }
 
     /**
-     * リダイレクトさせる
-     * @param url リダイレクト先
+     * リダイレクトさせる。
+     * Redirect.
+     * @param url URL of the redirection destination.
      */
     redirect(url: string): void {
         this.response.status = 302;
