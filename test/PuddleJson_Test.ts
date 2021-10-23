47f95a238dd0253b21379d842b860cf1e84208c1
KINGVOXY <daruo0830@gmail.com>
2021-10-23 13:25:46 +0900

    作業中

--- test/PuddleJson_Test.ts
+++ test/PuddleJson_Test.ts
@@ -6,11 +6,38 @@
  */
 
 import { assertEquals, assertNotEquals } from "https://deno.land/std/testing/asserts.ts";
-import { PuddleJson } from "../mod.ts";
+import { PuddleJSON } from "../mod.ts";
+
+let USERS: PuddleJSON[] = [];
+
+try {
+    await Deno.remove("./run_Test/assets/test.json");
+} catch (e) {
+}
+
 
 Deno.test({
-    name: "testing example",
+    name: "USE",
     fn(): void {
-        
+        const users = PuddleJSON.USE("./run_test/assets/test.json", {
+            id: ["UNIQUE", "NOT NULL", "AUTO INCREMENT"],
+            name: ["NOT NULL"],
+            age: []
+        });
+
+        USERS.push(users);
+    },
+});
+
+Deno.test({
+    name: "INNSERT",
+    async fn(): Promise<void> {
+        USERS[0].INSERT({ name: "Steve", age: 20 });
+        const ans = `[
+{"name":"Steve","age":20,"id":1}
+]`
+
+        assertEquals(ans, await Deno.readTextFile("./run_Test/assets/test.json"))
+
     },
 });
