44a42b256a493d6b28b533a60f6bf6af553a9ea2
KINGVOXY <daruo0830@gmail.com>
2021-10-26 22:44:57 +0900

    PuddleJsonクラスの単体テスト

--- test/PuddleJson_Test.ts
+++ test/PuddleJson_Test.ts
@@ -19,7 +19,7 @@ try {
 
 
 Deno.test({
-    name: "USE",
+    name: "USE,CREATE",
     fn(): void {
         const users = PuddleJSON.USE("./run_Test/assets/test.json", {
             id: ["UNIQUE", "NOT NULL", "AUTO INCREMENT"],
@@ -28,9 +28,10 @@ Deno.test({
             psy: ["UNIQUE"]
         });
 
-        const visualArts = PuddleJSON.USE("./run_Test/assets/test2.json", {
+        const visualArts = PuddleJSON.CREATE("./run_Test/assets/test2.json", {
             id: ["UNIQUE", "NOT NULL", "AUTO INCREMENT"],
             name: ["NOT NULL"],
+            height: ["NOT NULL"],
             game: [],
         });
         
@@ -66,16 +67,16 @@ Deno.test({
             `\n[PuddleJSON]\nSchema error. "name" has null object.\nスキーマエラーです。"name"はNULLオブジェクトを持っています。`,
         );
 
-        PJ[1].INSERT({ name: "Yukito Kunisaki", game: "Air"   });
-        PJ[1].INSERT({ name: "Misuzu Kamio",    game: "Air"   });
-        PJ[1].INSERT({ name: "Kano Kirishima",  game: "Air"   });
-        PJ[1].INSERT({ name: "Minagi Tono",     game: "Air"   });
-        PJ[1].INSERT({ name: "Yuichi Aizawa",   game: "Kanon" });
-        PJ[1].INSERT({ name: "Nayuki Minase",   game: "Kanon" });
-        PJ[1].INSERT({ name: "Makoto Sawatari", game: "Kanon" });
-        PJ[1].INSERT({ name: "Shiori Misaka",   game: "Kanon" });
-        PJ[1].INSERT({ name: "Ayu Tsukimiya",   game: "Kanon" });
-        PJ[1].INSERT({ name: "Mai Kawasumi",    game: "Kanon" });
+        PJ[1].INSERT({ name: "Yukito Kunisaki", height: 185, game: "Air"   });
+        PJ[1].INSERT({ name: "Misuzu Kamio",    height: 159, game: "Air"   });
+        PJ[1].INSERT({ name: "Kano Kirishima",  height: 156, game: "Air"   });
+        PJ[1].INSERT({ name: "Minagi Tono",     height: 169, game: "Air"   });
+        PJ[1].INSERT({ name: "Yuichi Aizawa",   height: 173, game: "Kanon" });
+        PJ[1].INSERT({ name: "Nayuki Minase",   height: 164, game: "Kanon" });
+        PJ[1].INSERT({ name: "Makoto Sawatari", height: 159, game: "Kanon" });
+        PJ[1].INSERT({ name: "Shiori Misaka",   height: 157, game: "Kanon" });
+        PJ[1].INSERT({ name: "Ayu Tsukimiya",   height: 154, game: "Kanon" });
+        PJ[1].INSERT({ name: "Mai Kawasumi",    height: 167, game: "Kanon" });
     },
 });
 
@@ -110,3 +111,78 @@ Deno.test({
     }
 });
 
+Deno.test({
+    name: "SELECTIF",
+    fn(): void {
+        const res = PJ[1].SELECTIF(row=>({ height: Number(row.height) >= 180 })).RESULT();
+        assertEquals("Yukito Kunisaki", res[0].name);
+        assertEquals(undefined, res[1]);
+        
+        const res2 = PJ[1].SELECTIF(row=>({ height: Number(row.height) >= 150 && Number(row.height) < 160 })).RESULT();
+        assertEquals("Ayu Tsukimiya",   res2[0].name);
+        assertEquals("Shiori Misaka",   res2[1].name);
+        assertEquals("Makoto Sawatari", res2[2].name);
+        assertEquals("Kano Kirishima",  res2[3].name);
+        assertEquals("Misuzu Kamio",    res2[4].name);
+        assertEquals(undefined, res2[5]);
+        
+        const res3 = PJ[1].SELECTIF(row=>({ game: row.game == "Air" })).RESULT();
+        assertEquals("Minagi Tono",     res3[0].name);
+        assertEquals("Kano Kirishima",  res3[1].name);
+        assertEquals("Misuzu Kamio",    res3[2].name);
+        assertEquals("Yukito Kunisaki", res3[3].name);
+        assertEquals(undefined, res3[4]);
+
+        const res4 = PJ[1].SELECTIF(row=>({ game: String(row.name).includes("Ka") })).RESULT();
+        assertEquals("Mai Kawasumi",    res4[0].name);
+        assertEquals("Kano Kirishima",  res4[1].name);
+        assertEquals("Misuzu Kamio",    res4[2].name);
+        
+    }
+});
+
+Deno.test({
+    name: "autoIncrement",
+    fn(): void {
+        const results = PJ[1].SELECT().RESULT("id");
+        
+        let i = results.length;
+        results.forEach(result => {
+            assertEquals(i, result.id);
+            i--;
+        });
+
+    }
+});
+
+Deno.test({
+    name: "parse",
+    fn(): void {
+        type SCHEMA = {[key:string]:("UNIQUE"|"AUTO INCREMENT"|"NOT NULL")[]|string;};
+        // const sos = PuddleJSON.USE("./run_Test/assets/test3.json", {
+        //     id: ["UNIQUE", "NOT NULL", "AUTO INCREMENT"],
+        //     name: ["UNIQUE", "NOT NULL"],
+        // });
+        
+        const row = [
+            {name: "Haruhi Suzumiya", id: 5},
+            {name: "Kyon",            id: 4},
+            {name: "Yuki Nagato",     id: 3},
+            {name: "Mikuru Asahina",  id: 2},
+            {name: "Itsuki Koizumi",  id: 1},
+        ];
+
+        const schema: SCHEMA = {
+            id: ["UNIQUE", "NOT NULL", "AUTO INCREMENT"],
+            name: ["UNIQUE", "NOT NULL"],
+        };
+
+        const results = PuddleJSON.checkSchema(row, schema);
+        let i = 5;
+        results.forEach(result => {
+            assertEquals(i, result.id);
+            i--;
+        });
+
+    }
+});
