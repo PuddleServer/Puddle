a9268c93d6b4e1c7240b191d0ee4d9f8e7cf2948
KINGVOXY <daruo0830@gmail.com>
2021-12-03 23:25:25 +0900

    PATCHまで

--- test/_RouterAndController/Route_Test.ts
+++ test/_RouterAndController/Route_Test.ts
@@ -12,7 +12,10 @@ import {
 import {
     Route,
     default_get,
-    default_error
+    default_error,
+    SystemRequest,
+    SystemResponse,
+    HandlerFunction
 } from "../../mod.ts";
 
 Deno.test({
@@ -82,3 +85,176 @@ Deno.test({
     }
 });
 
+Deno.test({
+    name: "GET1",
+    fn(): void {
+        const route = new Route("test_GET1");
+
+        assertStrictEquals(default_get().toString(), route.GET().toString());
+    },
+});
+
+Deno.test({
+    name: "GET2",
+    fn(): void {
+        const route = new Route("test_GET2");
+        const handler: HandlerFunction = (req: SystemRequest, res: SystemResponse) => {
+            res.setText("Collect!"); 
+        }
+        route.GET(handler);
+
+        assertStrictEquals(handler.toString(), route.GET().toString());
+    },
+});
+
+Deno.test({
+    name: "GET3",
+    fn(): void {
+        const route = new Route("test_GET3");
+        const process = new Response();
+        const handler = ()=>process;
+        route.GET(process);
+
+        assertStrictEquals(handler.toString(), route.GET().toString());
+    },
+});
+
+Deno.test({
+    name: "PUT1",
+    fn(): void {
+        const route = new Route("test_PUT1");
+        const process_404 = default_error(404, `Not Found. 見つかりません。`);
+
+        assertStrictEquals(process_404.toString(), route.PUT().toString());
+    },
+});
+
+Deno.test({
+    name: "PUT2",
+    fn(): void {
+        const route = new Route("test_PUT2");
+        const handler: HandlerFunction = (req: SystemRequest, res: SystemResponse) => {
+           res.setText("Collect!"); 
+        }
+        route.PUT(handler);
+
+        assertStrictEquals(handler.toString(), route.PUT().toString());
+    },
+});
+
+Deno.test({
+    name: "PUT3",
+    fn(): void {
+        const route = new Route("test_PUT3");
+        const process = new Response();
+        const handler = ()=>process;
+        route.PUT(process);
+
+        assertStrictEquals(handler.toString(), route.PUT().toString());
+    },
+});
+
+Deno.test({
+    name: "POST1",
+    fn(): void {
+        const route = new Route("test_POST1");
+        const process_404 = default_error(404, `Not Found. 見つかりません。`);
+
+        assertStrictEquals(process_404.toString(), route.POST().toString());
+    },
+});
+
+Deno.test({
+    name: "POST2",
+    fn(): void {
+        const route = new Route("test_POST2");
+        const handler: HandlerFunction = (req: SystemRequest, res: SystemResponse) => {
+           res.setText("Collect!"); 
+        }
+        route.POST(handler);
+
+        assertStrictEquals(handler.toString(), route.POST().toString());
+    },
+});
+
+Deno.test({
+    name: "POST3",
+    fn(): void {
+        const route = new Route("test_POST3");
+        const process = new Response();
+        const handler = ()=>process;
+        route.POST(process);
+
+        assertStrictEquals(handler.toString(), route.POST().toString());
+    },
+});
+
+Deno.test({
+    name: "DELETE1",
+    fn(): void {
+        const route = new Route("test_DELETE1");
+        const process_404 = default_error(404, `Not Found. 見つかりません。`);
+
+        assertStrictEquals(process_404.toString(), route.DELETE().toString());
+    },
+});
+
+Deno.test({
+    name: "DELETE2",
+    fn(): void {
+        const route = new Route("test_DELETE2");
+        const handler: HandlerFunction = (req: SystemRequest, res: SystemResponse) => {
+           res.setText("Collect!"); 
+        }
+        route.DELETE(handler);
+
+        assertStrictEquals(handler.toString(), route.DELETE().toString());
+    },
+});
+
+Deno.test({
+    name: "DELETE3",
+    fn(): void {
+        const route = new Route("test_DELETE3");
+        const process = new Response();
+        const handler = ()=>process;
+        route.DELETE(process);
+
+        assertStrictEquals(handler.toString(), route.DELETE().toString());
+    },
+});
+
+Deno.test({
+    name: "PATCH1",
+    fn(): void {
+        const route = new Route("test_PATCH1");
+        const process_404 = default_error(404, `Not Found. 見つかりません。`);
+
+        assertStrictEquals(process_404.toString(), route.PATCH().toString());
+    },
+});
+
+Deno.test({
+    name: "PATCH2",
+    fn(): void {
+        const route = new Route("test_PATCH2");
+        const handler: HandlerFunction = (req: SystemRequest, res: SystemResponse) => {
+           res.setText("Collect!"); 
+        }
+        route.PATCH(handler);
+
+        assertStrictEquals(handler.toString(), route.PATCH().toString());
+    },
+});
+
+Deno.test({
+    name: "PATCH3",
+    fn(): void {
+        const route = new Route("test_PATCH3");
+        const process = new Response();
+        const handler = ()=>process;
+        route.PATCH(process);
+
+        assertStrictEquals(handler.toString(), route.PATCH().toString());
+    },
+});
