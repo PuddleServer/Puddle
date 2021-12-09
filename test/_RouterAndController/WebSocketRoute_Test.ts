f65cbc4787d728b7455010d505bf1314ec1bd4f1
KINGVOXY <daruo0830@gmail.com>
2021-12-09 21:47:18 +0900

    ねこねこにゃーん

--- test/_RouterAndController/WebSocketRoute_Test.ts
+++ test/_RouterAndController/WebSocketRoute_Test.ts
@@ -2,56 +2,134 @@
  * WebSocketRouter.tsのWebSocketRouteクラステストファイル
  * @author Daruo(KINGVOXY)
  * @author AO2324(AO2324-00)
- * @Date   2021-10-01
+ * @Date   2021-12-09
  */
 
-import { assertEquals, assertNotEquals }        from "../mod_test.ts";
-import { WebSocketRoute, default_onopen, default_onmessage } from "../../mod.ts";
-
-let wsr: WebSocketRoute;
-let wsr2: WebSocketRoute;
+ import {
+    assertEquals,
+    assertStrictEquals
+} from "../mod_test.ts";
+import {
+    WebSocketRoute,
+    WebSocketClient,
+    default_onopen,
+    default_onmessage
+} from "../../mod.ts";
 
 Deno.test({
-    name: "wsRoute生成",
+    name: "constructor1",
     fn(): void {
-        wsr = new WebSocketRoute({
-            onopen: function (): string {
-                return "open";
-            },
-            onclose: function (): string {
-                return "close";
-            },
-            onmessage: function (): string {
-                return "message"
-            },
+        const websocketRoute = new WebSocketRoute();
+
+        assertEquals(websocketRoute instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onopen()), JSON.stringify(default_onopen));
+        assertStrictEquals(JSON.stringify(websocketRoute.onmessage()), JSON.stringify(default_onmessage));
+    },
+});
+
+Deno.test({
+    name: "constructor2",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute({
+            onopen:     event,
+            onclose:    event,
+            onmessage:  event,
+            onerror:    event    
         });
 
-        wsr2 = new WebSocketRoute();
-
+        assertEquals(websocketRoute instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onopen()), JSON.stringify(event));
+        assertStrictEquals(JSON.stringify(websocketRoute.onclose()), JSON.stringify(event));
+        assertStrictEquals(JSON.stringify(websocketRoute.onmessage()), JSON.stringify(event));
+        assertStrictEquals(JSON.stringify(websocketRoute.onerror()), JSON.stringify(event));
     },
 });
 
 Deno.test({
-    name: "onopenテスト",
+    name: "onopen1",
     fn(): void {
-        assertEquals("open", wsr.onopen()());
-        assertEquals(default_onopen.toString(), wsr2.onopen().toString());
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute({onopen: event});
+
+        assertStrictEquals(JSON.stringify(websocketRoute.onopen()), JSON.stringify(event));
     },
 });
 
 Deno.test({
-    name: "oncloseテスト",
+    name: "onopen2",
     fn(): void {
-        assertEquals("close", wsr.onclose()());
-        assertEquals("function () { }", wsr2.onclose().toString())
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute();
+        websocketRoute.onopen(event);
+
+        assertEquals(websocketRoute.onopen(event) instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onopen()), JSON.stringify(event));
     },
 });
 
 Deno.test({
-    name: "onmessageテスト",
+    name: "onclose1",
     fn(): void {
-        assertEquals("message", wsr.onmessage()());
-        assertEquals(default_onmessage.toString(), wsr2.onmessage().toString());
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute({onclose: event});
+
+        assertStrictEquals(JSON.stringify(websocketRoute.onclose()), JSON.stringify(event));
     },
 });
 
+Deno.test({
+    name: "onclose2",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute();
+        websocketRoute.onclose(event);        
+
+        assertEquals(websocketRoute.onclose(event) instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onclose()), JSON.stringify(event));
+    },
+});
+
+Deno.test({
+    name: "onmessage1",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute({onmessage: event}); 
+
+        assertStrictEquals(JSON.stringify(websocketRoute.onmessage()), JSON.stringify(event));
+    },
+});
+
+Deno.test({
+    name: "onmessage2",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute();
+        websocketRoute.onmessage(event);
+
+        assertEquals(websocketRoute.onmessage(event) instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onmessage()), JSON.stringify(event));
+    },
+});
+
+Deno.test({
+    name: "onerror1",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute({onerror: event});        
+
+        assertStrictEquals(JSON.stringify(websocketRoute.onerror()), JSON.stringify(event));
+    },
+});
+
+Deno.test({
+    name: "onerror2",
+    fn(): void {
+        const event = (client: WebSocketClient)=>console.log(client);
+        const websocketRoute = new WebSocketRoute();
+        websocketRoute.onerror(event);     
+
+        assertEquals(websocketRoute.onerror(event) instanceof WebSocketRoute, true);
+        assertStrictEquals(JSON.stringify(websocketRoute.onerror()), JSON.stringify(event));
+    },
+});
